79050cef1492a71bd63943af8ebc954b6fb76d8d
Ebenezerlalu07 <155541997+Ebenezerlalu07@users.noreply.github.com>
2026-06-18 15:03:47 +0530

    Add files via upload
    files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,32 @@
+import type { Metadata } from "next";
 import { Michroma } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "../components/SmoothScroll";
 import Navbar from "@/components/navbar";
 
-
 const michroma = Michroma({
   subsets: ["latin"],
   weight: "400",
 });
+
+export const metadata: Metadata = {
+  title: "German Luxury Automotive",
+  description:
+    "Connecting premium German automotive brands worldwide. Explore partnerships, innovation, and luxury experiences with Mercedes-Benz, BMW, Audi, Porsche, Volkswagen, and Alpina.",
+  keywords: [
+    "Mercedes-Benz",
+    "BMW",
+    "Audi",
+    "Porsche",
+    "Volkswagen",
+    "Alpina",
+    "German Luxury Cars",
+    "Automotive Network",
+  ],
+
+  
+};
+
 
 export default function RootLayout({
   children,
